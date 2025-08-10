aa074ec7eb45ca5f88f6cbb2dab66764343e82fa
4gari <58832262+4gari@users.noreply.github.com>
2025-08-10 18:26:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-// Cambiar imágenes al pasar el mouse
+// Hover cambio de imagen
 const img1 = document.getElementById('img1');
 const img2 = document.getElementById('img2');
 
-img1.addEventListener('mouseover', () => img1.src = 'imagenes/imagen3.jpg');
-img1.addEventListener('mouseout', () => img1.src = 'imagenes/imagen1.jpg');
+img1.addEventListener('mouseover', () => img1.src = 'imagenes-victor/imagen3.jpg');
+img1.addEventListener('mouseout', () => img1.src = 'imagenes-victor/imagen1.jpg');
 
-img2.addEventListener('mouseover', () => img2.src = 'imagenes/imagen4.jpg');
-img2.addEventListener('mouseout', () => img2.src = 'imagenes/imagen2.jpg');
+img2.addEventListener('mouseover', () => img2.src = 'imagenes-marcos/imagen4.jpg');
+img2.addEventListener('mouseout', () => img2.src = 'imagenes-marcos/imagen2.jpg');
+
+// Click para ir a los portafolios
+img1.addEventListener('click', () => {
+    window.location.href = 'victor.html';
+});
+img2.addEventListener('click', () => {
+    window.location.href = 'marcos.html';
+});
