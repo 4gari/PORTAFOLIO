bad03a502944ddc5291abf48d8c35fc471b05fef
4gari <58832262+4gari@users.noreply.github.com>
2025-08-10 18:17:36 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 const img1 = document.getElementById('img1');
 const img2 = document.getElementById('img2');
 
-img1.addEventListener('mouseover', () => img1.src = 'imagenes/i3.jpg');
-img1.addEventListener('mouseout', () => img1.src = 'imagenes/i1.jpg');
+img1.addEventListener('mouseover', () => img1.src = 'imagenes/imagen3.jpg');
+img1.addEventListener('mouseout', () => img1.src = 'imagenes/imagen1.jpg');
 
-img2.addEventListener('mouseover', () => img2.src = 'imagenes/i4.jpg');
-img2.addEventListener('mouseout', () => img2.src = 'imagenes/i2.jpg');
+img2.addEventListener('mouseover', () => img2.src = 'imagenes/imagen4.jpg');
+img2.addEventListener('mouseout', () => img2.src = 'imagenes/imagen2.jpg');
